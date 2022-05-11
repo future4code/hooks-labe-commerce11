2e510de4be17dff383b0a333123d35691ca3c5e9
GabrielePeron <gabrieleperon84@gmail.com>
2022-05-11 12:52:37 -0300

    mexi nos cards de produto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import './App.css';
-import Border from './components/Border';
-
-
-
+import React from 'react'
+import './App.css'
+import Border from './components/Border'
 
 function App() {
-  return (
-      <Border/>
-  );
+  return <Border />
 }
 
-export default App;
+export default App
